552b1405a96ebdaa51cb5c18192641ee8987801a
Tomas Jira <tom.jira@gmail.com>
2024-10-28 15:04:16 +0100

    add the ability to generate other fractals with the mandelbrot script

--- src/cpp/mandelbrot.cpp
+++ src/cpp/mandelbrot.cpp
@@ -7,8 +7,59 @@
 
 unsigned int nthread = 1;
 
+template <typename T>
+struct FractalFunctions {
+    std::unordered_map<std::string, std::function<std::tuple<std::complex<T>, std::complex<T>>(const std::complex<T>&, const std::complex<T>&, const std::complex<T>&, const std::complex<T>&)>> content = {
+        {"buffalo", [](const std::complex<T>& p, const std::complex<T>& z, const std::complex<T>& zp, const std::complex<T>& c) {
+            std::complex<T> abs_z = {abs(z.real()), abs(z.imag())}; return std::make_tuple(abs_z * abs_z - abs_z + p, z);
+        }},
+        {"burningship", [](const std::complex<T>& p, const std::complex<T>& z, const std::complex<T>& zp, const std::complex<T>& c) {
+            std::complex<T> abs_z = {abs(z.real()), abs(z.imag())}; return std::make_tuple(abs_z * abs_z + p, z);
+        }},
+        {"julia", [](const std::complex<T>& p, const std::complex<T>& z, const std::complex<T>& zp, const std::complex<T>& c) {
+            return std::make_tuple(z * z + c, z);
+        }},
+        {"mandelbrot", [](const std::complex<T>& p, const std::complex<T>& z, const std::complex<T>& zp, const std::complex<T>& c) {
+            return std::make_tuple(z * z + p, z);
+        }},
+        {"manowar", [](const std::complex<T>& p, const std::complex<T>& z, const std::complex<T>& zp, const std::complex<T>& c) {
+            return std::make_tuple(z * z + zp + p, z);
+        }},
+        {"phoenix", [](const std::complex<T>& p, const std::complex<T>& z, const std::complex<T>& zp, const std::complex<T>& c) {
+            return std::make_tuple(z * z - T(0.5) * zp + T(0.5667), z);
+        }}
+    };
+};
+
+template <typename T>
+struct FractalInitialConditions {
+    std::unordered_map<std::string, std::function<std::tuple<std::complex<T>, std::complex<T>, std::complex<T>>(const std::complex<T>&, const std::complex<T>&)>> content = {
+        {"buffalo",     [](const std::complex<T>& p, const std::complex<T>& c) {return std::make_tuple(p, T(0), T(0));}},
+        {"burningship", [](const std::complex<T>& p, const std::complex<T>& c) {return std::make_tuple(p, T(0), T(0));}},
+        {"julia",       [](const std::complex<T>& p, const std::complex<T>& c) {return std::make_tuple(p, p, T(0));}},
+        {"mandelbrot",  [](const std::complex<T>& p, const std::complex<T>& c) {return std::make_tuple(p, T(0), T(0));}},
+        {"manowar",     [](const std::complex<T>& p, const std::complex<T>& c) {return std::make_tuple(p, p, p);}},
+        {"phoenix",     [](const std::complex<T>& p, const std::complex<T>& c) {return std::make_tuple(p, p, c);}}
+    };
+};
+
+template <typename T>
+struct TrapFunctions {
+    std::vector<std::function<T(const std::complex<T>&)>> content = {
+        [](const std::complex<T>& p) {return std::norm(p);},
+        [](const std::complex<T>& p) {return std::min(abs(p.real()), abs(p.imag()));},
+        [](const std::complex<T>& p) {return std::min(abs(p.real() - p.imag()), abs(p.real() + p.imag())) / sqrt(2);},
+        [](const std::complex<T>& p) {return abs(std::norm(p) - 1.0);},
+        [](const std::complex<T>& p) {return std::min(std::norm(p), abs(std::norm(p) - 1.0));}
+    }; 
+};
+
 struct Image {
     std::vector<unsigned char> data; unsigned int width, height;
+};
+
+struct Fractal {
+    std::string name; double parameter;
 };
 
 struct Density {
@@ -24,7 +75,10 @@ struct Trap {
 };
 
 template <typename T>
-void draw_density(Image& image, const std::complex<T>& center, T zoom, const Density& density_algorithm) {
+void draw_density(Image& image, const Fractal& fractal, const std::complex<T>& center, T zoom, const Density& density_algorithm) {
+    auto fractal_function    = FractalFunctions<T>()        .content.at(fractal.name);
+    auto fractal_ic_function = FractalInitialConditions<T>().content.at(fractal.name);
+
     std::uniform_real_distribution<double> uniform_real(-3.5, 2.0), uniform_imag(-1.9, 1.9); std::mt19937 mt(density_algorithm.seed);
     std::vector<unsigned int> data(image.width * image.height, 0); std::vector<std::vector<unsigned int>> thread_data(nthread, data);
 
@@ -33,10 +87,12 @@ void draw_density(Image& image, const std::complex<T>& center, T zoom, const Den
     #pragma omp parallel for num_threads(nthread)
     for (const std::complex<T>& p : ps) {
 
-        std::complex<T> z = T(0); std::vector<std::complex<T>> orbit;
+        std::complex<T> parameter = exp(std::complex<T>{0, 1} * T(fractal.parameter));
+
+        auto [pi, z, zp] = fractal_ic_function(p, parameter); int n; T v; std::vector<std::complex<T>> orbit;
 
         for(int n = 0; n < density_algorithm.max_iterations; n++) {
-            z = z * z + p; if (std::norm(z) > density_algorithm.bailout_radius * density_algorithm.bailout_radius) break; orbit.push_back(z);
+            std::tie(z, zp) = fractal_function(pi, z, zp, parameter); if (std::norm(z) > density_algorithm.bailout_radius * density_algorithm.bailout_radius) break; orbit.push_back(z);
         }
 
         if (std::norm(z) > density_algorithm.bailout_radius * density_algorithm.bailout_radius) for (const std::complex<T>& o : orbit) {
@@ -61,17 +117,22 @@ void draw_density(Image& image, const std::complex<T>& center, T zoom, const Den
 }
 
 template <typename T>
-void draw_escape(Image& image, const std::complex<T>& center, T zoom, const Escape& escape_algorithm) {
+void draw_escape(Image& image, const Fractal& fractal, const std::complex<T>& center, T zoom, const Escape& escape_algorithm) {
+    auto fractal_function    = FractalFunctions<T>().content.at(fractal.name);
+    auto fractal_ic_function = FractalInitialConditions<T>().content.at(fractal.name);
+
     #pragma omp parallel for collapse(2) num_threads(nthread)
     for (int i = 0; i < image.height; i++) for (int j = 0; j < image.width; j++) {
+
+        std::complex<T> parameter = exp(std::complex<T>{0, 1} * T(fractal.parameter));
 
         T im = -center.imag() + (3.0 * (i + 0.5) - 1.5 * image.height) / zoom / image.height;
         T re =  center.real() + (3.0 * (j + 0.5) - 1.5 * image.width)  / zoom / image.height;
 
-        std::complex<T> p = {re, im}, z = T(0); int n; T v;
+        auto [p, z, zp] = fractal_ic_function(std::complex<T>{re, im}, parameter); int n; T v;
 
         for (n = 0; n < escape_algorithm.max_iterations; n++) {
-            z = z * z + p; if (std::norm(z) > escape_algorithm.bailout_radius * escape_algorithm.bailout_radius) break;
+            std::tie(z, zp) = fractal_function(p, z, zp, parameter); if (std::norm(z) > escape_algorithm.bailout_radius * escape_algorithm.bailout_radius) break;
         }
 
         if (v = n; n < escape_algorithm.max_iterations && escape_algorithm.enable_smooth) v -= log2(0.5 * log(std::norm(z)));
@@ -85,25 +146,23 @@ void draw_escape(Image& image, const std::complex<T>& center, T zoom, const Esca
 }
 
 template <typename T>
-void draw_trap(Image& image, const std::complex<T>& center, T zoom, const Trap& trap_algorithm) {
-    static std::vector<std::function<T(const std::complex<T>&)>> traps {
-        [](const std::complex<T>& p) -> T {return std::norm(p);},
-        [](const std::complex<T>& p) -> T {return std::min(abs(p.real()), abs(p.imag()));},
-        [](const std::complex<T>& p) -> T {return std::min(abs(p.real() - p.imag()), abs(p.real() + p.imag())) / sqrt(2);},
-        [](const std::complex<T>& p) -> T {return abs(std::norm(p) - 1.0);},
-        [](const std::complex<T>& p) -> T {return std::min(std::norm(p), abs(std::norm(p) - 1.0));}
-    }; auto trap_function = traps.at(trap_algorithm.trap_index);
+void draw_trap(Image& image, const Fractal& fractal, const std::complex<T>& center, T zoom, const Trap& trap_algorithm) {
+    auto fractal_function    = FractalFunctions<T>()        .content.at(fractal.name             );
+    auto fractal_ic_function = FractalInitialConditions<T>().content.at(fractal.name             );
+    auto trap_function       = TrapFunctions<T>()           .content.at(trap_algorithm.trap_index);
 
     #pragma omp parallel for collapse(2) num_threads(nthread)
     for (int i = 0; i < image.height; i++) for (int j = 0; j < image.width; j++) {
 
+        std::complex<T> parameter = exp(std::complex<T>{0, 1} * T(fractal.parameter));
+
         T im = -center.imag() + (3.0 * (i + 0.5) - 1.5 * image.height) / zoom / image.height;
         T re =  center.real() + (3.0 * (j + 0.5) - 1.5 * image.width)  / zoom / image.height;
 
-        std::complex<T> p = {re, im}, z = T(0); std::vector<std::complex<T>> orbit;
+        auto [p, z, zp] = fractal_ic_function(std::complex<T>{re, im}, parameter); std::vector<std::complex<T>> orbit;
 
         for(int n = 0; n < trap_algorithm.max_iterations; n++) {
-            z = z * z + p; if (std::norm(z) > trap_algorithm.bailout_radius * trap_algorithm.bailout_radius) break; orbit.push_back(z);
+            std::tie(z, zp) = fractal_function(p, z, zp, parameter); if (std::norm(z) > trap_algorithm.bailout_radius * trap_algorithm.bailout_radius) break; orbit.push_back(z);
         }
 
         if (!trap_algorithm.fill_background || std::norm(z) > trap_algorithm.bailout_radius * trap_algorithm.bailout_radius) {
@@ -123,6 +182,7 @@ int main(int argc, char** argv) {
     program.add_argument("-c", "--center").help("-- Center of the image.").nargs(2).default_value(std::vector<std::string>{"-0.75", "0"});
     program.add_argument("-d", "--density").help("-- Density algorithm with number of iterations, bailout radius, number of samples and seed.").nargs(4).default_value(std::vector<std::string>{"80", "10", "1e7", "1"});
     program.add_argument("-e", "--escape").help("-- Escape algorithm with number of iterations, bailout radius and smooth boolean.").nargs(3).default_value(std::vector<std::string>{"80", "10", "1"});
+    program.add_argument("-f", "--fractal").help("-- Name of the fractal to generate and its parameter if needed.").nargs(3).default_value(std::vector<std::string>{"mandelbrot", "0"});
     program.add_argument("-h", "--help").help("-- This help message.").default_value(false).implicit_value(true);
     program.add_argument("-m", "--mpfr").help("-- Number of bits to represent a MPFR real number.").default_value(64U).scan<'i', unsigned int>();
     program.add_argument("-n", "--nthread").help("-- Number of threads to use.").default_value(1U).scan<'i', unsigned int>();
@@ -139,11 +199,13 @@ int main(int argc, char** argv) {
         throw std::runtime_error("YOU CAN USE ONLY ONE ALGORITHM AT A TIME");
     }
 
-    Image image; Density density_algorithm; Escape escape_algorithm; Trap trap_algorithm; std::complex<mpfr::mpreal> center; nthread = program.get<unsigned int>("--nthread");
+    Image image; Fractal fractal; Density density_algorithm; Escape escape_algorithm; Trap trap_algorithm; std::complex<mpfr::mpreal> center; nthread = program.get<unsigned int>("--nthread");
 
     image.width  = program.get<std::vector<unsigned int>>("--resolution").at(0 );
     image.height = program.get<std::vector<unsigned int>>("--resolution").at(1 );
     image.data   = std::vector<unsigned char>(3 * image.width * image.height, 0);
+
+    fractal.name = program.get<std::vector<std::string>>("--fractal").at(0), fractal.parameter = std::stod(program.get<std::vector<std::string>>("--fractal").at(1));
 
     density_algorithm.max_iterations = std::stoi(program.get<std::vector<std::string>>("--density").at(0));
     density_algorithm.bailout_radius = std::stod(program.get<std::vector<std::string>>("--density").at(1));
@@ -166,23 +228,14 @@ int main(int argc, char** argv) {
     for (int i = 0; i < nthread; i++) mpfr::mpreal::set_default_prec(program.get<unsigned int>("--mpfr"));
 
     if (program.is_used("--density")) {
-        if (program.is_used("--mpfr")) {
-            draw_density<mpfr::mpreal>(image, center, program.get("--zoom"), density_algorithm);
-        } else {
-            draw_density<double>(image, {center.real().toDouble(), center.imag().toDouble()}, std::stod(program.get("--zoom")), density_algorithm);
-        }
+        if (program.is_used("--mpfr")) draw_density<mpfr::mpreal>(image, fractal, center, program.get("--zoom"), density_algorithm);
+        else draw_density<double>(image, fractal, {center.real().toDouble(), center.imag().toDouble()}, std::stod(program.get("--zoom")), density_algorithm);
     } else if (program.is_used("--trap")) {
-        if (program.is_used("--mpfr")) {
-            draw_trap<mpfr::mpreal>(image, center, program.get("--zoom"), trap_algorithm);
-        } else {
-            draw_trap<double>(image, {center.real().toDouble(), center.imag().toDouble()}, std::stod(program.get("--zoom")), trap_algorithm);
-        }
+        if (program.is_used("--mpfr")) draw_trap<mpfr::mpreal>(image, fractal, center, program.get("--zoom"), trap_algorithm);
+        else draw_trap<double>(image, fractal, {center.real().toDouble(), center.imag().toDouble()}, std::stod(program.get("--zoom")), trap_algorithm);
     } else {
-        if (program.is_used("--mpfr")) {
-            draw_escape<mpfr::mpreal>(image, center, program.get("--zoom"), escape_algorithm);
-        } else {
-            draw_escape<double>(image, {center.real().toDouble(), center.imag().toDouble()}, std::stod(program.get("--zoom")), escape_algorithm);
-        }
+        if (program.is_used("--mpfr")) draw_escape<mpfr::mpreal>(image, fractal, center, program.get("--zoom"), escape_algorithm);
+        else draw_escape<double>(image, fractal, {center.real().toDouble(), center.imag().toDouble()}, std::stod(program.get("--zoom")), escape_algorithm);
     }
 
     stbi_write_png(program.get("--output").c_str(), image.width, image.height, 3, image.data.data(), 3 * image.width);
