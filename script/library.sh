bee5bf9c606a1b9f72cbbf8fd44bf9cd417116d4
Tomas Jira <tom.jira@gmail.com>
2024-10-28 08:40:15 +0100

    change GMP mirror

--- script/library.sh
+++ script/library.sh
@@ -16,7 +16,7 @@ wget -O external/include/argparse.hpp https://raw.githubusercontent.com/p-ranav/
 wget -O external/include/stb_image_write.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image_write.h
 
 # download gmp
-wget -O external/libgmp.tar.xz https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz
+wget -O external/libgmp.tar.xz https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
 
 # unpack the archives
 cd external && for ARCHIVE in *.tar.xz; do tar -xf $ARCHIVE; done; cd ..
