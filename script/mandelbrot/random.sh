a26e393bcca790c93c01dd82c9221a3ee336e613
Tomas Jira <tom.jira@gmail.com>
2024-10-31 20:31:16 +0100

    improve the random.sh script

--- script/mandelbrot/random.sh
+++ script/mandelbrot/random.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-random() {
-    awk -v min=$1 -v max=$2 -v seed=$(date +%s%N) -v return_int=$3 'BEGIN {srand(seed); print (return_int == 1) ? int(min + rand() * (max - min + 1)) : min + rand() * (max - min)}'
+I=$(shuf -i 0-1000000000 -n 1); I0=$I; random() {
+    RANDOM=$4; awk -v min=$1 -v max=$2 -v seed=$RANDOM -v return_int=$3 'BEGIN {srand(seed); print (return_int == 1) ? int(min + rand() * (max - min + 1)) : min + rand() * (max - min)}'
 }
 
 # ======================================================================================================================================================================================================
@@ -16,7 +16,7 @@ generate() { RANDOM_FRACTAL=1; RANDOM_ALGORITHM=1; RANDOM_COLORING=1; RANDOM_TRA
 
 FRACTALS=("buffalo" "burningship" "julia" "mandelbrot" "manowar" "phoenix"); FRACTAL_INDEX=3
 
-[ $RANDOM_FRACTAL -eq 1 ] && FRACTAL_INDEX=$(random 0 5 1)
+[ $RANDOM_FRACTAL -eq 1 ] && FRACTAL_INDEX=$(random 0 5 1 $I) && I=$((I+=1))
 
 # ======================================================================================================================================================================================================
 # ALGORITHM
@@ -24,18 +24,18 @@ FRACTALS=("buffalo" "burningship" "julia" "mandelbrot" "manowar" "phoenix"); FRA
 
 ALGORITHMS=("density" "escape" "trap"); ALGORITHM_INDEX=1
 
-[ $FRACTAL_INDEX -eq 0 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 0 2 1)
-[ $FRACTAL_INDEX -eq 1 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 0 2 1)
-[ $FRACTAL_INDEX -eq 2 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 1 2 1)
-[ $FRACTAL_INDEX -eq 3 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 0 2 1)
-[ $FRACTAL_INDEX -eq 4 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 1 2 1)
-[ $FRACTAL_INDEX -eq 5 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 1 2 1)
+[ $FRACTAL_INDEX -eq 0 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 0 2 1 $I) && I=$((I+=1))
+[ $FRACTAL_INDEX -eq 1 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 0 2 1 $I) && I=$((I+=1))
+[ $FRACTAL_INDEX -eq 2 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 1 2 1 $I) && I=$((I+=1))
+[ $FRACTAL_INDEX -eq 3 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 0 2 1 $I) && I=$((I+=1))
+[ $FRACTAL_INDEX -eq 4 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 1 2 1 $I) && I=$((I+=1))
+[ $FRACTAL_INDEX -eq 5 ] && [ $RANDOM_ALGORITHM -eq 1 ] && ALGORITHM_INDEX=$(random 1 2 1 $I) && I=$((I+=1))
 
 SAMPLES=10000000
 
 TRAP_INDEX=2; FILL=0
-[ $RANDOM_TRAP -eq 1 ] && TRAP_INDEX=$(random 0 4 1)
-[ $RANDOM_FILL -eq 1 ] &&       FILL=$(random 0 1 1)
+[ $RANDOM_TRAP -eq 1 ] && TRAP_INDEX=$(random 0 4 1 $I) && I=$((I+=1))
+[ $RANDOM_FILL -eq 1 ] &&       FILL=$(random 0 1 1 $I) && I=$((I+=1))
 
 # ======================================================================================================================================================================================================
 # COLORING
@@ -43,94 +43,48 @@ TRAP_INDEX=2; FILL=0
 
 COLORINGS=("linear" "periodic" "solid"); COLORING_INDEX=1
 
-[ $ALGORITHM_INDEX -eq 0 ] && [ $RANDOM_COLORING -eq 1 ] && COLORING_INDEX=$(random 0 2 1)
-[ $ALGORITHM_INDEX -eq 1 ] && [ $RANDOM_COLORING -eq 1 ] && COLORING_INDEX=$(random 0 2 1)
-[ $ALGORITHM_INDEX -eq 2 ] && [ $RANDOM_COLORING -eq 1 ] && COLORING_INDEX=$(random 0 2 1)
+[ $ALGORITHM_INDEX -eq 0 ] && [ $RANDOM_COLORING -eq 1 ] && COLORING_INDEX=$(random 0 2 1 $I) && I=$((I+=1))
+[ $ALGORITHM_INDEX -eq 1 ] && [ $RANDOM_COLORING -eq 1 ] && COLORING_INDEX=$(random 0 2 1 $I) && I=$((I+=1))
+[ $ALGORITHM_INDEX -eq 2 ] && [ $RANDOM_COLORING -eq 1 ] && COLORING_INDEX=$(random 0 2 1 $I) && I=$((I+=1))
 
 LINEAR_1=0; LINEAR_2=0; LINEAR_3=0; LINEAR_4=255; LINEAR_5=255; LINEAR_6=255;
-[ $RANDOM_LINEAR -eq 1 ] && LINEAR_1=$(random 0 255 1)
-[ $RANDOM_LINEAR -eq 1 ] && LINEAR_2=$(random 0 255 1)
-[ $RANDOM_LINEAR -eq 1 ] && LINEAR_3=$(random 0 255 1)
-[ $RANDOM_LINEAR -eq 1 ] && LINEAR_4=$(random 0 255 1)
-[ $RANDOM_LINEAR -eq 1 ] && LINEAR_5=$(random 0 255 1)
-[ $RANDOM_LINEAR -eq 1 ] && LINEAR_6=$(random 0 255 1)
+[ $RANDOM_LINEAR -eq 1 ] && LINEAR_1=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_LINEAR -eq 1 ] && LINEAR_2=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_LINEAR -eq 1 ] && LINEAR_3=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_LINEAR -eq 1 ] && LINEAR_4=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_LINEAR -eq 1 ] && LINEAR_5=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_LINEAR -eq 1 ] && LINEAR_6=$(random 0 255 1 $I) && I=$((I+=1))
 
 SOLID_1=255; SOLID_2=255; SOLID_3=255
-[ $RANDOM_SOLID -eq 1 ] && SOLID_1=$(random 0 255 1)
-[ $RANDOM_SOLID -eq 1 ] && SOLID_2=$(random 0 255 1)
-[ $RANDOM_SOLID -eq 1 ] && SOLID_3=$(random 0 255 1)
+[ $RANDOM_SOLID -eq 1 ] && SOLID_1=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_SOLID -eq 1 ] && SOLID_2=$(random 0 255 1 $I) && I=$((I+=1))
+[ $RANDOM_SOLID -eq 1 ] && SOLID_3=$(random 0 255 1 $I) && I=$((I+=1))
 
 PERIODIC_1=31.93; PERIODIC_2=6.26; PERIODIC_3=30.38; PERIODIC_4=5.86; PERIODIC_5=11.08; PERIODIC_6=0.81
-[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_1=$(random 0 50.0 0)
-[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_2=$(random 0 6.28 0)
-[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_3=$(random 0 50.0 0)
-[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_4=$(random 0 6.28 0)
-[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_5=$(random 0 50.0 0)
-[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_6=$(random 0 6.28 0)
+[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_1=$(random 0 50.0 0 $I) && I=$((I+=1))
+[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_2=$(random 0 6.28 0 $I) && I=$((I+=1))
+[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_3=$(random 0 50.0 0 $I) && I=$((I+=1))
+[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_4=$(random 0 6.28 0 $I) && I=$((I+=1))
+[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_5=$(random 0 50.0 0 $I) && I=$((I+=1))
+[ $RANDOM_PERIODIC -eq 1 ] && PERIODIC_6=$(random 0 6.28 0 $I) && I=$((I+=1))
 
 # ======================================================================================================================================================================================================
 # POSITION
 # ======================================================================================================================================================================================================
 
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.35 && CENTER_IMAG=0.35 && ZOOM=0.80 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.35 && CENTER_IMAG=0.35 && ZOOM=0.80 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.35 && CENTER_IMAG=0.35 && ZOOM=0.80 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.70 && CENTER_IMAG=0.45 && ZOOM=0.90 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.70 && CENTER_IMAG=0.45 && ZOOM=0.90 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.25 && CENTER_IMAG=0.30 && ZOOM=1.10 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.70 && CENTER_IMAG=0.45 && ZOOM=0.90 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.70 && CENTER_IMAG=0.45 && ZOOM=0.90 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 0 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.25 && CENTER_IMAG=0.30 && ZOOM=1.10 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 0 ] && CENTER_REAL=-0.45 && CENTER_IMAG=0.45 && ZOOM=0.90 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 1 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
+[ $FRACTAL_INDEX -eq 3 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 4 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 5 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
 
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.20 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 0 ] && [ $COLORING_INDEX  -eq 2 ] && CENTER_REAL=-0.35 && CENTER_IMAG=0.35 && ZOOM=1.00 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 0 ] && CENTER_REAL=-0.10 && CENTER_IMAG=0.20 && ZOOM=1.00 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 1 ] && [ $ALGORITHM_INDEX -eq 2 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.35 && ZOOM=1.00 && PARAMETER=0.00
+[ $FRACTAL_INDEX -eq 4 ] && [ $COLORING_INDEX  -eq 2 ] && CENTER_REAL=-0.10 && CENTER_IMAG=0.00 && ZOOM=8.00 && PARAMETER=0.00
 
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-[ $FRACTAL_INDEX -eq 2 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=1.57
-
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 3 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.75 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-[ $FRACTAL_INDEX -eq 4 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-1.30 && CENTER_IMAG=0.00 && ZOOM=1.20 && PARAMETER=0.00
-
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 1 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 0 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 1 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
-[ $FRACTAL_INDEX -eq 5 ] && [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && CENTER_REAL=-0.00 && CENTER_IMAG=0.00 && ZOOM=1.65 && PARAMETER=3.14
+[ $FRACTAL_INDEX -eq 1 ] && [ $COLORING_INDEX  -eq 2 ] && [ $ALGORITHM_INDEX != 0 ] && CENTER_REAL=-0.60 && CENTER_IMAG=0.60 && ZOOM=1.30 && PARAMETER=0.00
 
 # ======================================================================================================================================================================================================
 # SAMPLE
@@ -144,7 +98,7 @@ PERIODIC_1=31.93; PERIODIC_2=6.26; PERIODIC_3=30.38; PERIODIC_4=5.86; PERIODIC_5
 
 [ $ALGORITHM_INDEX -eq 0 ] && [ $COLORING_INDEX -eq 2 ] && SAMPLES=1000000
 [ $ALGORITHM_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && FILL=1
-[ $FRACTAL_INDEX -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && exit 1
+[ $FRACTAL_INDEX   -eq 2 ] && [ $COLORING_INDEX -eq 2 ] && exit 1
 
 # ======================================================================================================================================================================================================
 # PARAMETERS
@@ -157,4 +111,4 @@ COLORING_PARAMS=("-l $LINEAR_1 $LINEAR_2 $LINEAR_3 $LINEAR_4 $LINEAR_5 $LINEAR_6
 # GENERATION
 # ======================================================================================================================================================================================================
 
-set -x && ./bin/mandelbrot -c $CENTER_REAL $CENTER_IMAG -f ${FRACTALS[$FRACTAL_INDEX]} $PARAMETER -z $ZOOM ${ALGORITHM_PARAMS[$ALGORITHM_INDEX]} ${COLORING_PARAMS[$COLORING_INDEX]}
+echo -n "RANDOM=$I0 " && set -x && ./bin/mandelbrot -c $CENTER_REAL $CENTER_IMAG -f ${FRACTALS[$FRACTAL_INDEX]} $PARAMETER -z $ZOOM ${ALGORITHM_PARAMS[$ALGORITHM_INDEX]} ${COLORING_PARAMS[$COLORING_INDEX]}
